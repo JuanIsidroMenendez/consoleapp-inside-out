87d8400bcef71e86b979083bf8a5c24816d20c83
Juan <juan.isidro.menendez@gmail.com>
2026-08-06 13:38:54 +0200

    feat: add Emotion enum with display names

---
+++ src/main/java/dev/juanim/Emotion.java
@@ -0,0 +1,13 @@
+package dev.juanim;
+
+public enum Emotion {
+    JOY("Alegría"),
+    SADNESS("Tristeza"),
+    ANGER("Ira"),
+    DISGUST("Asco"),
+    FEAR("Miedo"),
+    ENVY("Envidia"),
+    EMBARRASSMENT("Vergüenza"),
+    BOREDOM("Aburrimiento"),
+    NOSTALGIA("Nostalgia"),
+}
