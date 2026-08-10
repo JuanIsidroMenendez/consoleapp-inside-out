6e8aa334707b2946748082c8e8893c1cd0010010
Juan <juan.isidro.menendez@gmail.com>
2026-08-10 10:24:09 +0200

    docs: add reference comments to MomentService

--- src/main/java/dev/juanim/services/MomentService.java
+++ src/main/java/dev/juanim/services/MomentService.java
@@ -11,24 +11,25 @@ import dev.juanim.repositories.InMemoryMomentRepository;
 public class MomentService {
 
     private final InMemoryMomentRepository repository;
-
+    /* Constructor */
     public MomentService(InMemoryMomentRepository repository) {
         this.repository = repository;
     }
-
+    /* Añadir momento */
     public Moment addMoment(String title, String description, Emotion emotion, LocalDate momentDate) {
         int id = repository.generateId();
         Moment moment = new Moment(id, title, description, emotion, momentDate);
         return repository.save(moment);
     }
-
+    /* Listar todos los momentos */
     public List<Moment> getAllMoments() {
         return repository.findAll();
     }
-
+    /* Eliminar momento por id */
     public boolean deleteMoment(int id) {
     return repository.deleteById(id);
-}
+    }
+    /*Obtener/Filtrar momentos por emoción */
     public List<Moment> getMomentsByEmotion(Emotion emotion) {
         List<Moment> result = new ArrayList<>();
         for (Moment moment : repository.findAll()) {
