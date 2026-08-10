fc4aacee0731c24f1361ee62981401bb016fac96
Juan <juan.isidro.menendez@gmail.com>
2026-08-10 09:19:41 +0200

    feat: add getAllMoments to MomentService

--- src/main/java/dev/juanim/services/MomentService.java
+++ src/main/java/dev/juanim/services/MomentService.java
@@ -1,6 +1,7 @@
 package dev.juanim.services;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import dev.juanim.models.Emotion;
 import dev.juanim.models.Moment;
@@ -18,5 +19,9 @@ public class MomentService {
         int id = repository.generateId();
         Moment moment = new Moment(id, title, description, emotion, momentDate);
         return repository.save(moment);
+    }
+
+    public List<Moment> getAllMoments() {
+        return repository.findAll();
     }
 }
